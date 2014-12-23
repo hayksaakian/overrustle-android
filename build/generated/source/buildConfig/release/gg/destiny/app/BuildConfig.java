ec91d18ba7f2f4ca5054e14c49fcf6b640d41fdd
Hayk Saakian <hayk@skyrealre.com>
2014-12-23 15:34:46 -0800

    exclude org.json to avoid conflict

--- build/generated/source/buildConfig/release/gg/destiny/app/BuildConfig.java
+++ build/generated/source/buildConfig/release/gg/destiny/app/BuildConfig.java
@@ -5,9 +5,9 @@ package gg.destiny.app;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
-  public static final String PACKAGE_NAME = "gg.destiny.app";
+  public static final String APPLICATION_ID = "gg.destiny.app";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 511;
+  public static final int VERSION_CODE = 520;
   public static final String VERSION_NAME = "";
 }
