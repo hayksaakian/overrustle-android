53af49e52ab2a936cf663fc3efd13c765d60376b
Hayk Saakian <hayk@skyrealre.com>
2015-04-14 20:47:39 -0700

    add watch face for android wear

--- build/generated/source/buildConfig/debug/gg/destiny/app/BuildConfig.java
+++ build/generated/source/buildConfig/debug/gg/destiny/app/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "gg.destiny.app";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 590;
+  public static final int VERSION_CODE = 591;
   public static final String VERSION_NAME = "";
 }
